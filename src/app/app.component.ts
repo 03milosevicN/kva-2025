6fa5fcc95038eaa54312a008b5dc487223c7f80c
03milosevicN <nikolamilosevic220803@gmail.com>
2025-02-28 10:55:05 +0100

    Fixed gap in app.component.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {MatSlideToggle} from '@angular/material/slide-toggle';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, MatToolbar, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
